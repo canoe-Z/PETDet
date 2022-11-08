c5dc8986859d6d125ab2b75ffc84d73c32d306e0
liwentao0705 <liwentao0705@outlook.com>
2022-11-09 01:34:54 +0800

    add auto_scale_lr

--- configs/_base_/default_runtime.py
+++ configs/_base_/default_runtime.py
@@ -17,6 +17,10 @@ workflow = [('train', 1)]
 opencv_num_threads = 0
 # set multi-process start method as `fork` to speed up the training
 mp_start_method = 'fork'
-
-#fp16
+# Default setting for scaling LR automatically
+#   - `enable` means enable scaling LR automatically
+#       or not by default.
+#   - `base_batch_size` = (8 GPUs) x (2 samples per GPU).
+auto_scale_lr = dict(enable=True, base_batch_size=8)
+# default enable fp16
 fp16 = dict(loss_scale='dynamic')
